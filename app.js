c3a350b087cbd9f5f43a8029de69390f13e921cb
dhruv-gupta-dev <heroicdhruv007@gmail.com>
2026-01-07 22:40:27 +0530

    Added updateValues function

--- app.js
+++ app.js
@@ -45,3 +45,23 @@ function addTransactionDOM(transaction) {
 
     ls.appendChild(item);
 }
+
+function updateValues() {
+    const amounts = arr.map(transaction => transaction.amount);
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+    const expense = (amounts
+        .filter(item => item < 0)
+        .reduce((acc, item) => (acc += item), 0) * -1)
+        .toFixed(2);
+
+    balance.innerText = `$${total}`;
+    moneyPlus.innerText = `$${income}`;
+    moneyMinus.innerText = `$${expense}`;
+}
